26f18f15ec9adb4828593af06034efcdc5df9e73
syler <hileo77@gmail.com>
2019-09-29 15:12:39 +0200

    addded version check

--- src/cli.ts
+++ src/cli.ts
@@ -3,6 +3,8 @@ import { failure } from './messages';
 import { Watch } from './watch';
 import { Build } from './build';
 import { Init } from './init';
+import chalk from 'chalk';
+import { readFile } from 'fs-extra';
 
 class Start {
   args: string[];
@@ -14,7 +16,7 @@ class Start {
     });
     this.run();
   }
-  private run(): void {
+  private async run() {
     switch (this.args[0]) {
       case 'watch':
       case '--w':
@@ -33,6 +35,10 @@ class Start {
       case '-i':
       case 'i':
         Init(this.args[1]);
+        break;
+      case '-v':
+        const file = await readFile('package.json');
+        console.log(chalk.green(`v${JSON.parse(file.toString()).version} Node: ${process.version}`));
         break;
       default:
         failure('Error! No Command Given.');
